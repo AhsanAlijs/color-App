4605727d97dc9f32f3d64867cbb4c7d67ee5d9d9
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-09-02 02:25:28 -0700

    Add Css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const display = document.getElementById("color")
-const color = ["Green","Red","Blue","purple"];
+const color = ["Green","Red","Blue","Purple"];
 // console.log(`${color}`);
-display.innerHTML+=`<li><button>${color.slice(0,1)}</button></li>`
-display.innerHTML+=`<li><button>${color.slice(1,2)}</button></li>`
-display.innerHTML+=`<li><button>${color.slice(2,3)}</button></li>`
-display.innerHTML+=`<li><button>${color.slice(3,4)}</button></li>`
+display.innerHTML+=`<li><button class="green">${color.slice(0,1)}</button></li>`
+display.innerHTML+=`<li><button class="red">${color.slice(1,2)}</button></li>`
+display.innerHTML+=`<li><button class="blue">${color.slice(2,3)}</button></li>`
+display.innerHTML+=`<li><button class="purple">${color.slice(3,4)}</button></li>`
